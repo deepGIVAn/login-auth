ef380c68f7ac50c05e2627038a9007f17ff53c4a
deepGIVAn <88539737+deepGIVAn@users.noreply.github.com>
2023-08-26 19:49:38 +0530

    login-server-checked

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   ATLAS_URL:
     "mongodb+srv://givan:givan@login-otp.asslffd.mongodb.net/login-otp?retryWrites=true&w=majority",
   EMAIL_API_KEY:
-    "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",
+    "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",
 };
 
 // works in git only..
